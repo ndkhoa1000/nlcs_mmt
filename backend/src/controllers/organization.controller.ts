02c3429a06c5019e9a3894df5096adef882fc5bb
L8duck <ndkhoa1000@gmail.com>
2025-04-03 11:59:38 +0700

    feat: add delete endpoint

--- backend/src/controllers/organization.controller.ts
+++ backend/src/controllers/organization.controller.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
 import { HTTPSTATUS } from "../config/http.config";
-import { createOrganizationSchema } from "../validation/organization.validation";
-import { createOrganizationService, getAllOrganizationsUserIsMemberService } from "../services/organization.service";
+import { changeRoleSchema, createOrganizationSchema, updateOrganizationSchema } from "../validation/organization.validation";
+import { changeOrganizationMemberRoleService, createOrganizationService, deleteOrganizationByIdService, getAllOrganizationsUserIsMemberService, getOrganizationAnalyticsService, getOrganizationByIdService, getOrganizationMembersService, updateOrganizationByIdService } from "../services/organization.service";
+import { objectIdSchema } from "../validation/common.validation";
+import { getMemberRoleInWorkspaceService } from "../services/member.service";
+import { roleGuard } from "../utils/roleGuard";
+import { Permissions } from "../enums/roles.enums";
+
 
 export const createOrganizationController = asyncHandler(
     async (req: Request, res: Response) => {
@@ -27,57 +32,100 @@ export const getAllOrganizationsUserIsMemberController = asyncHandler(
         });
     }
 );
+
 export const getOrganizationByIdController = asyncHandler(
     async (req: Request, res: Response) => {
-        const body = createOrganizationSchema.parse({ ...req.body });
+        const orgId = objectIdSchema.parse(req.params.id);
 
+        const {organization} = await getOrganizationByIdService(orgId);
         return res.status(HTTPSTATUS.OK).json({
             message: "Organization updated.",
+            organization
+        });
+    }
+);
+export const getOrganizationMembersController = asyncHandler(
+    //REVIEW: need to test on postman.
+    async (req: Request, res: Response) => {
+        const userId = String(req.user?._id);
+        const orgId = objectIdSchema.parse(req.params.id);
+
+        const {role} = await getMemberRoleInWorkspaceService(userId,orgId);
+        roleGuard(role, [Permissions.VIEW_ONLY]);
+
+        const {members, roles} = await getOrganizationMembersService(orgId);
+
+        return res.status(HTTPSTATUS.OK).json({
+            message: "Fetch",
+            members,
+            roles,
         });
     }
 );
 export const updateOrganizationByIdController = asyncHandler(
     async (req: Request, res: Response) => {
-        // const body
+        const userId = String(req.user?._id);
+        const orgId = objectIdSchema.parse(req.params.id);
+        const body = updateOrganizationSchema.parse({...req.body});
+        const {role} = await getMemberRoleInWorkspaceService(userId,orgId);
+        roleGuard(role, [Permissions.EDIT_ORGANIZATION]);
+
+        const {organization} = await updateOrganizationByIdService(userId, orgId, body);
 
         return res.status(HTTPSTATUS.OK).json({
             message: "Organization updated.",
+            organization
         });
     }
 );
-export const changeOrganizationMemberRoleController = asyncHandler(
-    async (req: Request, res: Response) => {
-        // const body
-
-        return res.status(HTTPSTATUS.OK).json({
-            message: "Organization updated.",
-        });
-    }
-);
-export const deleteOrganizationByIdController = asyncHandler(
-    async (req: Request, res: Response) => {
-        // const body
-
-        return res.status(HTTPSTATUS.OK).json({
-            message: "Organization updated.",
-        });
-    }
-);
-export const getOrganizationMembersController = asyncHandler(
-    async (req: Request, res: Response) => {
-        // const body
-
-        return res.status(HTTPSTATUS.OK).json({
-            message: "Organization updated.",
-        });
-    }
-);
+// TODO: comeback when finish other resource.
 export const getOrganizationAnalyticsController = asyncHandler(
     async (req: Request, res: Response) => {
-        // const body
+        const userId = String(req.user?._id);
+        const orgId = objectIdSchema.parse(req.params.id);
+       
+        const {role} = await getMemberRoleInWorkspaceService(userId,orgId);
+        roleGuard(role, [Permissions.VIEW_ONLY]);
+
+        // const {organization} = await getOrganizationAnalyticsService(userId, orgId);
 
         return res.status(HTTPSTATUS.OK).json({
             message: "Organization updated.",
+        });
+    }
+);
+
+export const changeOrganizationMemberRoleController = asyncHandler(
+    async (req: Request, res: Response) => {
+        const userId = String(req.user?._id);
+        const orgId = objectIdSchema.parse(req.params.id);
+        const {memberId, roleId} = changeRoleSchema.parse(req.body);
+
+        const {role} = await getMemberRoleInWorkspaceService(userId,orgId);
+        roleGuard(role, [Permissions.CHANGE_MEMBER_ROLE]);
+
+        const {member} = await changeOrganizationMemberRoleService(orgId, memberId, roleId);
+
+        return res.status(HTTPSTATUS.OK).json({
+            message: "member's role updated.",
+            member
+        });
+    }
+);
+
+// REVIEW: delete Org will delete everything related to Org, finish other models to perform fully delete.
+export const deleteOrganizationByIdController = asyncHandler(
+    async (req: Request, res: Response) => {
+        const userId = String(req.user?._id);
+        const orgId = objectIdSchema.parse(req.params.id);
+
+        const {role} = await getMemberRoleInWorkspaceService(userId,orgId);
+        roleGuard(role, [Permissions.DELETE_ORGANIZATION]);
+
+        const {currentOrg} = await deleteOrganizationByIdService(userId, orgId);
+        return res.status(HTTPSTATUS.OK).json({
+            message: "Organization deleted.",
+            currentOrg
         });
     }
 );
--- backend/src/services/organization.service.ts
+++ backend/src/services/organization.service.ts
@@ -1,13 +1,15 @@
 
 import mongoose from "mongoose";
 import OrganizationModel from "../models/organization.model";
-import {ConflictException, NotFoundException } from "../utils/appError";
+import {BadRequestException, ConflictException, NotFoundException } from "../utils/appError";
 import UserModel from "../models/user.model";
 import RoleModel from "../models/roles-permission.model";
 import { Roles } from "../enums/roles.enums";
 import MemberModel from "../models/member.model";
+import ProgramModel from "../models/program.model";
+import EventModel from "../models/event.model";
+import AttendanceModel from "../models/attendance.model";
 
-// TODO [x]: Create workspace -> create member(workspaceId, userId, "OWNER")
 // NOTE: may not need transaction for simple service.
 export const createOrganizationService = async(
     userId: string, 
@@ -17,11 +19,13 @@ export const createOrganizationService = async(
         phoneNumber: string,
         description?: string | null,
         mission?: string | null,
+        logo?:string |null,
         email?: string | null,
         website?: string | null,
         socialMediaLink?: string[] | null,
+        establishedDate?: Date | null,
 }) => {
-    const {name,address,phoneNumber,description,mission,email,website,socialMediaLink} = body;
+    const {name,address,phoneNumber,description,mission,logo,email,website,socialMediaLink, establishedDate} = body;
     const session = await mongoose.startSession();
     session.startTransaction();
     console.log('[createOrganizationService]: Start session...');
@@ -42,14 +46,15 @@ export const createOrganizationService = async(
                 phoneNumber,
                 description,
                 mission,
+                logo,
                 email,
                 website,
                 socialMediaLink,
                 isVerified: true, //NOTE: only for simplify Web App, need Verify feature
-                establishedDate: Date.now(),
+                establishedDate,
                 owner: userId
             });
-
+            
             await organization.save({session});
 
             let member = new MemberModel({
@@ -60,12 +65,17 @@ export const createOrganizationService = async(
 
             await member.save({session});
             
+            // update user's current org
+            user.currentOrganization = organization ? 
+            (organization._id as mongoose.Types.ObjectId) : user.currentOrganization;
+            await user.save({session});
+            
             await session.commitTransaction();
-            console.log('[createOrganizationService]: commit transaction...');
-            session.endSession();
-            console.log('[createOrganizationService]: session end. Finsh.');
-
-            return {organization};
+                console.log('[createOrganizationService]: commit transaction...');
+                session.endSession();
+                console.log('[createOrganizationService]: session end. Finish.');
+    
+                return {organization};
         }else{
             throw new ConflictException("Organization name has been taken. Please try another name!");
         }
@@ -89,3 +99,143 @@ export const getAllOrganizationsUserIsMemberService = async(userId: string) => {
 
     return {organizations};
 };
+
+export const getOrganizationByIdService = async (orgId: string) => {
+    const organization = await OrganizationModel.findById(orgId)
+
+    if (!organization)
+        throw new NotFoundException("Organization not found.");
+
+    //NOTE: return org and its member
+    const member = await MemberModel.find({orgId})
+    .populate("role")
+    .exec();
+    const organizationWithMember = {...organization.toObject(), member};
+    
+    return {organization: organizationWithMember};
+}   
+
+export const getOrganizationMembersService = async (orgId: string) => {
+    //NOTE: fetch all member from the organization
+
+    const members = MemberModel.find({orgId})
+    .populate("userId", "name email profilePicture -password")
+    .populate("role", "name");
+
+    const roles = RoleModel.find({}, {name:1, _id:1})
+    .select("-permission")
+    .lean();
+
+    return {members, roles};
+}
+
+export const updateOrganizationByIdService = async (
+    userId: string, 
+    orgId: string,
+    body :{
+        name?: string | null,
+        address?: string | null,
+        phoneNumber?: string | null,
+        description?: string | null,
+        mission?: string | null,
+        logo?:string |null,
+        email?: string | null,
+        website?: string | null,
+        socialMediaLink?: string[] | null,
+        establishedDate?: Date | null,
+}) => {
+    const {name, address, phoneNumber,description,mission,logo,email,website,socialMediaLink, establishedDate} = body;
+    const organization = await OrganizationModel.findOne({_id: orgId});
+
+    if(!organization)
+        throw new NotFoundException("Organization not found.");
+    organization.name = name ||organization.name;
+    organization.address = address || organization.address;
+    organization.phoneNumber = phoneNumber || organization.phoneNumber;
+    organization.description = description || organization.description;
+    organization.mission = mission || organization.mission;
+    organization.logo = logo || organization.logo;
+    organization.email = email || organization.email;
+    organization.website = website || organization.website;
+    organization.socialMediaLink = socialMediaLink || organization.socialMediaLink;
+    organization.establishedDate = establishedDate || organization.establishedDate;
+    await organization.save();
+    console.log(`Update organization ${organization._id} successfully`);
+
+    return {organization}
+}
+
+// REVIEW: test changeMemberRole with Postman when finish joinOrg API(member)
+export const changeOrganizationMemberRoleService = async(
+    orgId: string, 
+    memberId: string,
+    roleId: string,
+) => {
+    const role = await RoleModel.findById(roleId);
+    if(!role)
+        throw new NotFoundException("Role not found.")
+    
+    const member = await MemberModel.findOne({userId:memberId, orgId:orgId})
+    if(!member)
+        throw new NotFoundException("Member not found.")
+
+    member.role = role || member.role;
+    await member.save();
+    return {member}
+}
+
+// NOTE: need program and event for data analysis.
+// ideas for analysis:: 
+// member: totals members, roles distribution, new members
+// event: total events, event status, avg volunteer participation
+// program: total programs, program engagement.
+export const getOrganizationAnalyticsService = async(userId: string, orgId: string) => {
+    const organization = OrganizationModel.findOne({_id: orgId});
+}
+
+export const deleteOrganizationByIdService = async(userId: string, orgId: string) =>{
+    // NOTE: (transaction) check user is owner -> delete attendance -> delete event 
+    // NOTE:  delete project-> delete member -> update user.currentOrg -> delete Org
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    console.log('[deleteOrganizationService]: Start session...');
+    try {
+        const organization = await OrganizationModel.findById(orgId).session(session);
+        if(!organization)
+            throw new NotFoundException("Organization not found.");
+
+        // check user is owner
+        if(organization.owner.toString() != userId)
+            throw new BadRequestException("Yo are not authorized to delete this organization.");
+        
+        const events = await EventModel.find({organization: orgId}).session(session);
+        
+        events.every(async(event) => await AttendanceModel.deleteMany({eventId: event._id}).session(session));
+        await EventModel.deleteMany({organization: orgId}).session(session);
+        await ProgramModel.deleteMany({organization: orgId}).session(session);
+        await MemberModel.deleteMany({orgId}).session(session);
+        
+        const user = await UserModel.findById(userId).session(session);
+        if(!user)
+             throw new NotFoundException("User not found.");
+        
+        if(user?.currentOrganization?.equals(orgId)){
+            // find all one org that user is a part of (this org has been deleted)
+            const MemberOrg = await MemberModel.findOne({userId}).session(session);
+            user.currentOrganization = MemberOrg ? MemberOrg.orgId : null;
+            await user.save({session});
+        }
+
+        await OrganizationModel.deleteOne({_id: orgId}).session(session);
+        await session.commitTransaction();
+        session.endSession();
+        return {currentOrg: organization};
+    } catch (error) {
+        console.log("Error during session...", error);
+        await session.abortTransaction();
+        session.endSession();
+        throw error;
+    } finally {
+        session.endSession();
+    }
+}
