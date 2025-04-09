934b8e949a901a3b694ebf204564b0c7de03464b
L8duck <ndkhoa1000@gmail.com>
2025-04-09 21:02:57 +0700

    save state

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -1,5 +1,5 @@
 import { getEnv } from "../utils/get-env";
-
+// WARN: env might not read, so when deployment, need debug for ensure env can be read smoothly.
 const appConfig = () => ({
     NODE_ENV: getEnv("NODE_ENV", "development"),
     PORT:getEnv("PORT","5000"),
@@ -13,8 +13,8 @@ const appConfig = () => ({
     GOOGLE_CLIENT_SECRET: getEnv("GOOGLE_CLIENT_SECRET","some_secret"),
     GOOGLE_CALLBACK_URL: getEnv("GOOGLE_CALLBACK_URL","http://localhost:8000/api/auth/google/callback"),
 
-    FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "localhost"),
-    FRONTEND_GOOGLE_CALLBACK_URL: getEnv("FRONTEND_GOOGLE_CALLBACK_URL","test"),
+    FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "http://localhost:5173"),
+    FRONTEND_GOOGLE_CALLBACK_URL: getEnv("FRONTEND_GOOGLE_CALLBACK_URL","http://localhost:5173/google/oauth/callback"),
 })
 
 export const config = appConfig();
