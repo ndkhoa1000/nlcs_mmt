18301256c76c5f45867bfe7f55b864f9887ab0cc
L8duck <ndkhoa1000@gmail.com>
2025-04-10 17:17:11 +0700

    feat: add vietnamese font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,9 @@ export default {
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
   	extend: {
+		fontFamily: {
+			sans: ["Roboto Mono", "ui-sans-serif", "system-ui"],
+		  },
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
